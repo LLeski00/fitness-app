ef144dfb7cb950dbcb02db42b755ad25fa9ec90f
Luka <llesko00@fesb.hr>
2024-09-10 16:52:23 +0200

    Creating and outputting saved sessions

--- src/NewSession.js
+++ src/NewSession.js
@@ -5,6 +5,7 @@ import { FaMinus } from "react-icons/fa";
 
 const NewSession = () => {
     const [exercises, setExercises] = useState(null);
+    const [sessionName, setSessionName] = useState("Session name...");
 
     useEffect(() => {
         fetch("http://localhost:3001/exercises", {
@@ -34,28 +35,70 @@ const NewSession = () => {
         setExercises(temp);
     };
 
+    const handleSaveSession = () => {
+        let temp = { name: "Session", exercises: [] };
+        temp.name = sessionName;
+        temp.exercises = exercises.filter((exercise) => exercise.sets > 0);
+
+        fetch("http://localhost:3001/sessions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(temp),
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.error(error));
+
+        console.log(temp);
+        console.log(sessionName);
+    };
+
     return (
         <div className="new-session">
             <div className="new-session-content">
                 <h1>New Session</h1>
-                {exercises &&
-                    exercises.map((exercise) => (
-                        <div key={exercise.id} className="exercise">
-                            <h2>{exercise.name}</h2>
-                            <p>Sets: </p>
-                            <div className="set-counter">
-                                <FaMinus
-                                    onClick={() =>
-                                        handleMinusClick(exercise.id)
-                                    }
-                                />
-                                <p>{exercise.sets}</p>
-                                <FaPlus
-                                    onClick={() => handlePlusClick(exercise.id)}
-                                />
+                <div className="exercises">
+                    {exercises &&
+                        exercises.map((exercise) => (
+                            <div key={exercise.id} className="exercise">
+                                <h2>{exercise.name}</h2>
+                                <p>Sets: </p>
+                                <div className="set-counter">
+                                    <FaMinus
+                                        onClick={() =>
+                                            handleMinusClick(exercise.id)
+                                        }
+                                    />
+                                    <p>{exercise.sets}</p>
+                                    <FaPlus
+                                        onClick={() =>
+                                            handlePlusClick(exercise.id)
+                                        }
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                </div>
+
+                <p>Name this session:</p>
+
+                <div className="save-session">
+                    <input
+                        onChange={(e) => setSessionName(e.target.value)}
+                        value={sessionName}
+                        type="text"
+                        className="session-name"
+                    />
+
+                    <button
+                        className="save-session-button"
+                        onClick={() => handleSaveSession()}
+                    >
+                        Save
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/Train.js
+++ src/Train.js
@@ -1,10 +1,43 @@
+import { useState, useEffect } from "react";
 import "./style/Train.css";
 
 const Train = () => {
+    const [sessions, setSessions] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/sessions", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                setSessions(myJson);
+            });
+    }, []);
+
     return (
         <div className="train">
             <div className="train-content">
                 <h1>Train</h1>
+
+                <div className="sessions">
+                    {sessions &&
+                        sessions.map((session) => (
+                            <div key={session.id} className="session">
+                                <h2>{session.name}</h2>
+                                {session.exercises.map((exercise) => (
+                                    <div className="session-exercises">
+                                        <p>{exercise.name}</p>
+                                        <p>Sets: {exercise.sets}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                </div>
             </div>
         </div>
     );
